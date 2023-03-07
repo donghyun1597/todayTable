47241522a2154db301605ef5738d1534dcaed37d
donghyun1597 <donghyun1597@naver.com>
2023-03-07 15:18:27 +0900

    레시피작성수정

--- today/src/com/todayTable/recipe/controller/RecipeIngreInsertController.java
+++ today/src/com/todayTable/recipe/controller/RecipeIngreInsertController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RecipeIngreInsertController
  */
-@WebServlet("/ingreIn.re")
+@WebServlet("/ingreIn.rc")
 public class RecipeIngreInsertController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
